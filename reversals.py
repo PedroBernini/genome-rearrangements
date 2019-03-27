1647315ab3b495aadfe3f76a03bf588ba63153f4
Pedro Henrique B. Silva <37659137+PedroBernini@users.noreply.github.com>
2019-03-26 23:42:54 -0300

    Initial commit

---
+++ reversals.py
@@ -0,0 +1,10 @@
+import sys
+
+# ----- PERMUTAÇÃO (LINHA DE COMANDO) ----- #
+permutacao = []
+for el in range(1,len(sys.argv)) :
+    permutacao.append(sys.argv[el])
+print("\nPermutacao =", permutacao)
+
+
+
