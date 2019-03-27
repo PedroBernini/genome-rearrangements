486309fc4e787a06f3d5ee91928de0d330409cac
Pedro Henrique B. Silva <37659137+PedroBernini@users.noreply.github.com>
2019-03-27 00:46:42 -0300

    Add files via upload

--- reversals.py
+++ reversals.py
@@ -1,10 +1,25 @@
 import sys
 
 # ----- PERMUTAÇÃO (LINHA DE COMANDO) ----- #
-permutacao = []
-for el in range(1,len(sys.argv)) :
-    permutacao.append(sys.argv[el])
-print("\nPermutacao =", permutacao)
+permutacaoInicial = [0]
+permutacaoFinal = []
+n = len(sys.argv) - 1
+ultimo = None
+
+for el in range(1, n) :
+    permutacaoInicial.append(eval(sys.argv[el]))
+    if abs(eval(sys.argv[el])) == n - 1 :
+        if eval(sys.argv[el]) < 0 :
+            ultimo = (eval(sys.argv[el]) - 1) * -1
+        else :
+            ultimo = (eval(sys.argv[el]) + 1) * -1
+permutacaoInicial.append(ultimo)
+
+for el in range(0, n + 1) :
+    permutacaoFinal.append(el)
+
+print("\nPermutacao Inicial", permutacaoInicial)
+print("\nPermutacao Final  ", permutacaoFinal)
 
 
 
